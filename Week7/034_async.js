e9971ed5d536cf06985d57d86d3f435ea4dff9eb
Nayoung-Gu <skdud000322@naver.com>
2022-06-30 11:16:34 +0900

    0630_챌린지

---
+++ Week7/034_async.js
@@ -0,0 +1,16 @@
+const loadData = async () => {
+  try {
+    const url1 = "https://jsonplaceholder.typicode.com/todos/1";
+    const url2 = "https://jsonplaceholder.typicode.com/todos/2";
+    const url3 = "https://jsonplaceholder.typicode.com/todos/3";
+
+    const results = await Promise.all([fetch(url1), fetch(url2), fetch(url3)]);
+    const dataPromises = results.map((result) => result.json());
+    const finalData = await Promise.all(dataPromises);
+    console.log(finalData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadData();
